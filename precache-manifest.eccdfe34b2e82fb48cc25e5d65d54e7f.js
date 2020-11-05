d27ea901b11b4de6ccdd66ee819cdd3ee43ef8ae
Fighter <farmanara.code@gmail.com>
2020-11-06 00:42:18 +0330

    Updates

---
+++ precache-manifest.eccdfe34b2e82fb48cc25e5d65d54e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b4bd69a2e808727bf56d48a8858888",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "cdfa46d672057fb5b8a1",
+    "url": "/pomodoro/static/css/main.2110685e.chunk.css"
+  },
+  {
+    "revision": "d2ab2807b8d8af4b2c5d",
+    "url": "/pomodoro/static/js/2.d2ca10de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.d2ca10de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfa46d672057fb5b8a1",
+    "url": "/pomodoro/static/js/main.58efd7b6.chunk.js"
+  },
+  {
+    "revision": "65305f622151e1fb9cb8",
+    "url": "/pomodoro/static/js/runtime-main.bd81142a.js"
+  },
+  {
+    "revision": "54720affec067bea68f81250998e6cab",
+    "url": "/pomodoro/static/media/guitar.54720aff.mp3"
+  }
+]);
